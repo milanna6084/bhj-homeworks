de71f3666f9c1fc3bffb84ee2b7ab7e12cce0a0c
milanna6084 <molodojenyuk.ann@gmail.com>
2021-12-25 23:31:13 +0500

    Task 2. Popups

---
+++ element-search/popups/task.js
@@ -0,0 +1,21 @@
+const modalMainWindow = document.getElementById('modal_main');
+const modalSuccessWindow = document.getElementById('modal_success');
+const closeElements = document.getElementsByClassName('modal__close');
+const closeElementsCount = closeElements.length;
+const showSuccessElements = document.getElementsByClassName('show-success');
+const showSuccessElementsCount = showSuccessElements.length;
+
+modalMainWindow.className = 'modal modal_active';
+
+for (let i=0 ; i < closeElementsCount; i++) {
+    closeElements.item(i).onclick = () => { 
+        modalMainWindow.className = 'modal';
+        modalSuccessWindow.className = 'modal';
+    };
+}
+
+for (let i=0 ; i < showSuccessElementsCount; i++) {
+    showSuccessElements.item(i).onclick = () => {
+        modalSuccessWindow.className = 'modal modal_active';
+    };
+}
